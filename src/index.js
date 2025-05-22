fe8ee5cdd0659977b0863f610bec0359dda6f697
Sapana Gupta <146055257+sapanagupta25@users.noreply.github.com>
2025-05-22 17:50:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+console.log(store.getState());
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
