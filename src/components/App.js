ee2dd4739effed794f385f75d683dfbc409963d1
Sapana Gupta <146055257+sapanagupta25@users.noreply.github.com>
2025-05-22 17:43:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect } from "react";
+import "./../styles/App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { created } from "../dataSlice";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    const dispatch = useDispatch();
+    const  data  = useSelector((state) => state);
+    console.log(data);
+    useEffect(() => {
+        dispatch(created());
+    }, []);
+    return (
+        <div>
+            <div style={{ textAlign: "center" }}>
+                <h1> A short Naration of Lorem Ipsum</h1>
+                <h4>
+                    Below Contains A title and Body gotten from
+                    <br />a random API, Please take your time to Review
+                </h4>
+            </div>
+            <ul>
+                 {data &&
+                    data.map((post, idx) => {
+                        return (
+                            <li key={idx}>
+                                <div className="title">
+                                    <strong>Title</strong> :{post.title}
+                                </div>
+                                <br />
+                                <div className="body">
+                                    <strong>Body</strong> :{post.body}
+                                </div>
+                            </li>
+                        );
+                    })}
+            </ul>
+        </div>
+    );
+};
 
-export default App
+export default App;
